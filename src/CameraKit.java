92175325499549a22a2c1f3883e8b028c0a5d089
Kaan Korap <s35063@pjwstk.edu.pl>
2026-06-02 10:47:53 +0200

    Task 2: add equipment subclasses

---
+++ src/CameraKit.java
@@ -0,0 +1,30 @@
+public class CameraKit extends Equipment {
+
+    private int lensCount;
+    private boolean hasTripod;
+
+    public CameraKit(String id, String name, double baseDailyPrice,
+                     int lensCount, boolean hasTripod) {
+        super(id, name, baseDailyPrice);
+        this.lensCount = lensCount;
+        this.hasTripod = hasTripod;
+    }
+
+    @Override
+    public double calculateDailyPrice() {
+        double price = getBaseDailyPrice();
+
+        price += lensCount * 10;
+
+        if (hasTripod) {
+            price += 15;
+        }
+
+        return price;
+    }
+
+    @Override
+    public String getDetails() {
+        return lensCount + " lenses, tripod: " + hasTripod;
+    }
+}
